290f8181135aedc26e5aef6d339b63d489d3e09d
COSMAS SANYIWA <cosmase637@gmail.com>
2026-08-13 04:27:18 -0700

    Connect React frontend to Express backend via API

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require(`express`);
+const cors = require(`cors`);
 const app=express();
 const PORT = 5000;
+app.use(cors());
 app.get(`/`,(req,res)=>{res.send("Hello World from Express server!");});
+app.get(`/api/hello`,(req,res) => {res.json({message: `Hello World kutoka Node/Express backend!`});});
     app.listen(PORT,() => {
         console.log(`Server inaendesha kwenye http://localhost:${PORT}`);});
     
